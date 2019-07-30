fc1a912f6cf22b21fff5f3a6b582db52a2b2ddc3
gpdms960823@gmail.com <gpdms960823@gmail.com>
2019-07-30 16:17:59 -0700

    07301617

--- src/main/java/translation/tippersMaven/OntologyManager.java
+++ src/main/java/translation/tippersMaven/OntologyManager.java
@@ -34,11 +34,8 @@ public class OntologyManager {
    public static OWLOntology ontology;
    public static String ontologyURL = "C:\\Users\\KIM KI MIN\\Desktop\\research project\\ontology\\ontology.owl";
    public static OWLReasoner reasoner;
-//   public static DLQueryEngine queryEngine;
-   public static IRI ontologyIRI = IRI
-         .create("C:\\Users\\KIM KI MIN\\Desktop\\research project\\ontology\\ontology.owl");
+   public static IRI ontologyIRI = IRI.create("C:\\Users\\KIM KI MIN\\Desktop\\research project\\ontology\\ontology.owl");
    public static BidirectionalShortFormProvider bidiShortFormProvider;
-
    public OntologyManager() {
       startOntologyManager();
    }
@@ -55,16 +52,8 @@ public class OntologyManager {
 
          reasoner = createOWLReasoner();
 
-//         queryEngine = createDLQueryEngine();
-
          @SuppressWarnings("deprecation")
          Set<OWLOntology> importsClosure = ontology.getImportsClosure();
-         // Create a bidirectional short form provider to do the actual
-         // mapping.
-         // It will generate names using the input
-         // short form provider.
-//         bidiShortFormProvider = new BidirectionalShortFormProviderAdapter(manager, importsClosure, queryEngine.getShortFormProvider());
-
       } catch (OWLOntologyCreationException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
@@ -73,14 +62,7 @@ public class OntologyManager {
 
    // show information of ontology
    public static OWLOntology showOntology() throws OWLOntologyCreationException {
-//      OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-//      File file = new File("C:\\Users\\KIM KI MIN\\Desktop\\research project\\ontology\\ontology.owl");
-//      OWLOntology Ontology = manager.loadOntologyFromOntologyDocument(new File(ontologyURL));
-//      IRI documentIRI = manager.getOntologyDocumentIRI(Ontology);
-
       System.out.println("Loaded ontology: " + ontology);
-//      System.out.println("from: " + ontologyIRI);
-
       return ontology;
    }
 
@@ -95,7 +77,6 @@ public class OntologyManager {
    }
 
    // show subclasses
-   // ?낅젰???덉쿂 ?대옒?ㅼ쓽 ?쒕툕 ?대옒??諛쏆븘?ㅺ린
    public static void showSubclasses(String str) {
       System.out.println("\n print subclasses...");
       for (final OWLSubClassOfAxiom subClasse : ontology.getAxioms(AxiomType.SUBCLASS_OF)) {
@@ -105,11 +86,9 @@ public class OntologyManager {
             }
          }
       }
-   } // ?뚮씪誘명꽣濡?諛붽?寃?
-      // ?대? 肄붾뱶媛  ?덉쓬
+   }
 
    // get individuals
-   // ?낅젰???대옒?ㅼ쓽 individual 異쒕젰
    public static ArrayList<String> printIndividualsByclass(String owlClass) {
       System.out.println("\n show individuals...");
       ArrayList<String> instance = null;
@@ -130,8 +109,8 @@ public class OntologyManager {
       return instance;
    }
 
-   // ?낅젰???ㅽ듃留곴낵 媛숈? ?대쫫??objective property 媛  ?덉쑝硫?異쒕젰,
-   // ?꾨땲硫?null
+   //if there is object property
+   // return that property, else return null
    public static OWLObjectProperty getOntoobjProperty(String name) {
       for (OWLObjectProperty p : ontology.getObjectPropertiesInSignature())
          if (p.getIRI().getFragment().equals(name))
@@ -139,7 +118,8 @@ public class OntologyManager {
       return null;
    }
 
-   // ?낅젰???ㅽ듃留곴낵 媛숈? ?대쫫??data property 媛  ?덉쑝硫?異쒕젰, ?꾨땲硫?null
+   //if there is data property
+   // return that property, else return null
    public static OWLDataProperty getOntoDataProperty(String name) {
       for (OWLDataProperty p : ontology.getDataPropertiesInSignature())
          if (p.getIRI().getFragment().equals(name))
@@ -148,7 +128,6 @@ public class OntologyManager {
    }
 
    // findSensor
-   // observation??二쇰㈃ sensor瑜??섍꺼以?
    public static OWLObjectProperty findSensor(String string) {
       System.out.println("\n[Print Sensor by Observation]");
       OWLObjectProperty p = factory.getOWLObjectProperty(
@@ -163,10 +142,7 @@ public class OntologyManager {
                         && subProp.getSubProperty() instanceof OWLProperty) {
                      for (OWLObjectProperty o : subPrope.getSubProperty().getObjectPropertiesInSignature()) {
                         if (reasoner.getObjectPropertyRanges(o, true).toString().contains(string)) {
-                           System.out.println(reasoner.getObjectPropertyDomains(o, true).getFlattened()); // return
-                                                                                       					  // node
-                                                                                       					  // set
-                           
+                           System.out.println(reasoner.getObjectPropertyDomains(o, true).getFlattened()); // return node set
                            System.out.println("Sensor: " + reasoner.getObjectPropertyDomains(o, true)
                                  .getFlattened().iterator().next());
                         }
@@ -176,13 +152,11 @@ public class OntologyManager {
             }
          }
       }
-
       return null;
    }
 
    // is VS
    // find a type of specific sensor
-   // string 以??? 吏㏃? ?⑥뼱濡?以????덈룄濡??섏젙
    public static boolean isVS(String Sensor) {
       boolean flag = false;
       System.out.println("\n[Find Sensor Type]\n" + Sensor + "  is Virtual Sensor?");
@@ -210,7 +184,6 @@ public class OntologyManager {
 
       OWLReasonerFactory jfact = new JFactFactory();
       return jfact.createReasoner(ontology, new SimpleConfiguration(50000));
-      // return new Reasoner.ReasonerFactory().createReasoner(ontology);
    }
 
    /**
@@ -218,45 +191,6 @@ public class OntologyManager {
     * 
     * @param OWLOntology The ontology
     * @return DLQueryEngine The engine to process DL queries
-    */
-
-//   public static DLQueryEngine createDLQueryEngine() throws IllegalArgumentException {
-//      if (reasoner == null) {
-//         throw new IllegalArgumentException("OWLReasoner is null");
-//      }
-//      return new DLQueryEngine(reasoner, new SimpleShortFormProvider());
-//   }
-
-   /*
-    * 
-    * public void saveOntology(String nameOntology) { // Now save a local copy of
-    * the ontology. (Specify a path appropriate to // your setup)
-    * 
-    * File theDir = new File(HTMLParser.FOLDER_ONTOLOGIES);
-    * 
-    * if (!theDir.exists()) { try { theDir.mkdir(); } catch (SecurityException se)
-    * { // handle it } }
-    * 
-    * File file = new File(theDir.getPath() + File.separator + nameOntology +
-    * ".owl"); try { manager.saveOntology(ontology, IRI.create(file.toURI())); }
-    * catch (OWLOntologyStorageException e) { // TODO Auto-generated catch block
-    * e.printStackTrace(); }
-    * 
-    * // TO CHANGE FORMAT (IF NEEDED) // // By default ontologies are saved in the
-    * format from which they were // // loaded. In this case the ontology was
-    * loaded from rdf/xml. We // // can get information about the format of an
-    * ontology from its // manager // OWLDocumentFormat format =
-    * manager.getOntologyFormat(ontology); // // We can save the ontology in a
-    * different format. Lets save the // // ontology // // in owl/xml format //
-    * OWLXMLDocumentFormat owlxmlFormat = new OWLXMLDocumentFormat(); // // Some
-    * ontology formats support prefix names and prefix IRIs. In our // // case we
-    * loaded the Koala ontology from an rdf/xml format, which // // supports
-    * prefixes. When we save the ontology in the new format we // // will copy the
-    * prefixes over so that we have nicely abbreviated // IRIs // // in the new
-    * ontology document // if (format.isPrefixOWLOntologyFormat()) { //
-    * owlxmlFormat.copyPrefixesFrom(format.asPrefixOWLOntologyFormat()); // } //
-    * manager.saveOntology(ontology, owlxmlFormat, // IRI.create(file.toURI())); }
-    * 
     */
 
    public static void addAxiomsOntology(Set<OWLAxiom> axioms) {
