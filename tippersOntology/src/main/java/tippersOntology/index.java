566eb485a14a3ca26aa0271bf6f1c25c00cc3f2e
gpdms960823@gmail.com <gpdms960823@gmail.com>
2019-07-31 16:12:51 -0700

    final

---
+++ tippersOntology/src/main/java/tippersOntology/index.java
@@ -0,0 +1,23 @@
+package tippersOntology;
+
+import org.semanticweb.owlapi.model.OWLException;
+
+public class index {
+
+	public static void main(String[] args) throws OWLException {
+
+		OntologyManager.startOntologyManager();
+		OntologyManager.createOWLReasoner();
+		OntologyManager.showOntology();		//show ontology path
+		OntologyManager.showClasses();		//show ontology classes
+		OntologyManager.showSubclasses("http://www.semanticweb.org/kimkimin/ontologies/2019/6/untitled-ontology-12#Room");	//show all of subclasses with superclasses
+		OntologyManager.printIndividualsByclass("http://www.semanticweb.org/kimkimin/ontologies/2019/6/untitled-ontology-12#Sensor");
+		OntologyManager.findSensor("http://www.semanticweb.org/kimkimin/ontologies/2019/6/untitled-ontology-12#Occupancy");
+		OntologyManager.findObs("http://www.semanticweb.org/kimkimin/ontologies/2019/6/untitled-ontology-12#tempProp");
+
+		System.out.println(OntologyManager.isVS("Wifi"));
+		
+		System.out.println("\n" + OntologyManager.getOntoobjProperty("captures"));		//object property
+		System.out.println("\n" + OntologyManager.getOntoDataProperty("captures"));		//data property	
+	}
+}
