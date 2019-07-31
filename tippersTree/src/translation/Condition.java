4ab58706c49e2660b915db82d8f2ac87f46dbafd
gpdms960823@gmail.com <gpdms960823@gmail.com>
2019-07-31 14:36:11 -0700

    integrated

---
+++ tippersTree/src/translation/Condition.java
@@ -0,0 +1,14 @@
+package translation;
+
+import java.util.*;
+
+public class Condition {
+	public ArrayList<String> conds;
+	
+	public Condition() {
+		conds = new ArrayList<String>();
+		conds.add("Occupancy");
+		conds.add("Capacity");
+		conds.add("Location");
+	}
+}
---
+++ tippersTree/src/translation/UR.java
@@ -0,0 +1,13 @@
+package translation;
+
+public class UR {
+	public String Entity;
+	public String Condition;
+	
+	public UR() { }
+	
+	public UR(String newEntity, String newCondition) {
+		Entity = newEntity;
+		Condition = newCondition;
+	}
+}
