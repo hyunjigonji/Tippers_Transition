f05c546f5e0c1ffe8f93e5c79cd3077151293798
hyunjigonji <wew9955@naver.com>
2019-08-13 23:01:34 -0700

    finished checking feasibility algorithm

--- tippersTree2/src/main/java/tippersTree/Main.java
+++ tippersTree2/src/main/java/tippersTree/Main.java
@@ -17,31 +17,37 @@ public class Main {
 	public static void main(String args[]) {
 		OntologyManager.startOntologyManager();
 		
-		Tree Test = new Tree();
-		UA TestUA = new UA("meetingroom", "Turn on AC,Turn on Light", "Occupancy>50%Connectivity");
+		Tree myTree = new Tree();
+		UA myUA = new UA("meetingroom", "Turn on AC,Turn on Light", "Occupancy>50%Connectivity");
 		
-		Test = Tree_Flattening.flattening(TestUA);
-		//Tree_Display.displayTree(Test);
+		myTree = Tree_Flattening.flattening(myUA);
+		//Tree_Display.displayTree(myTree);
 		for(int i = 0 ; i < Tree.URij.size() ; i++) {
 			//System.out.println("hello");
 			URNode now = Tree.URij.get(i);
 			Tree_Generator.URgenerator0(now);
 		}
-		//Tree_Display.displayTree(Test);
+		//Tree_Display.displayTree(myTree);
 		for(int i = 0 ; i < Tree.UAij.size() ; i++) {
 			UANode now = Tree.UAij.get(i);
 			Tree_Generator.UAgenerator0(now);
 		}
-		//Tree_Display.displayTree(Test);
+		//Tree_Display.displayTree(myTree);
 		
 		// print leaf nodes
-		ArrayList<Node> leaves = Tree.findLeafNode(Test);
+		ArrayList<Node> leaves = Tree.findLeafNode(myTree);
 		for(int i = 0 ; i < leaves.size() ; i++) {
 			Node now = leaves.get(i);
-			SRNode now2 = Tree.findSRNode(Test, now.nodeNum);
-			Tree_Remove.checking(now2);
+			SRNode now2 = Tree.findSRNode(myTree, now.nodeNum);
+			Tree_Remove.check(now2);
 		}
-		Tree_Display.displayTree(Test);
+		Tree_Display.displayTree(myTree);
+		
+		// find feasible plan
+		Tree feasibleTree = new Tree(myUA);
+		feasibleTree = Tree_Calculate.check(myTree);
+		Tree_Display.displayTree(feasibleTree);
+		
 		
 		//Generator
 //		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
--- tippersTree2/src/main/java/tippersTree/Tree_Calculate.java
+++ tippersTree2/src/main/java/tippersTree/Tree_Calculate.java
@@ -2,34 +2,162 @@ package tippersTree;
 
 import java.util.*;
 
-/*
- * 배열 하나 만들어서 MAX로 초기화한 다음 각 노드당 최소값 리턴하기 
- */
-
 public class Tree_Calculate extends Tree {
 	public final static int MAX = 987654321;
 	
 	public final static int Wm = 1;
 	public final static int Wt = 2;
 	
-	public int Costs[] = {MAX,};
 	public static ArrayList<Node> Leaves;
 	
+	public static int Costs[] = new int[Tree.totalNum+1];
+	public static ArrayList<Integer> LeafNode[] = new ArrayList[Tree.totalNum+1];
+	public static ArrayList<Integer> Selected = new ArrayList<Integer>();
+	
 	public static Tree check(Tree myTree) {
-		Tree newTree = new Tree();
+		for(int i = 1 ; i <= Tree.totalNum ; i++) { // initialize the array with MAX value
+			Costs[i] = MAX;
+			LeafNode[i] = new ArrayList<Integer>();
+		}
+		
+		//Tree newTree = new Tree(); // How to generate newTree !!!
 		Leaves = findLeafNode(myTree);
 		
 		for(int i = 0 ; i < Leaves.size() ; i++) {
 			Node nowNode = Leaves.get(i);
-			SRNode nowSRNode = findSRNode(myTree, nowNode.nodeNum);
-			
-			calcul(nowSRNode);
+			calcul(myTree, nowNode, MAX, nowNode.nodeNum, new ArrayList<Integer>());
 		}
 		
-		return newTree;
+		for(int i = 0 ; i < URij.size() ; i++) {
+			for(int j = 0 ; j < LeafNode[URij.get(i).nodeNum].size() ; j++) {
+				Selected.add(LeafNode[URij.get(i).nodeNum].get(j));
+			}
+		}
+		for(int i = 0 ; i < UAij.size() ; i++) {
+			for(int j = 0 ; j < LeafNode[UAij.get(i).nodeNum].size() ; j++) {
+				Selected.add(LeafNode[UAij.get(i).nodeNum].get(j));
+			}
+		}
+		System.out.println(Selected);
+		//Selected.remove(Selected.indexOf(43));
+		//Selected.add(47);
+		//Selected.add(49);
+		
+		for(int i = 0 ; i < Leaves.size() ; i++) {
+			Node nowNode = Leaves.get(i);
+			if(!Selected.contains(nowNode.nodeNum)) remove(nowNode);
+		}
+		
+		return myTree;
 	}
 	
-	public static void calcul(SRNode nowNode) {
+	public static void calcul(Tree myTree, Node nowNode, int nowCost, int leafNum, ArrayList<Integer> leafNodeNum) {
+		System.out.println("now node : " + nowNode.nodeNum + " now cost: " + nowCost + " now leaf : " + leafNodeNum);
+		if(nowNode.type == types.typeDA) { // if DA, it is leaf and get cost.
+			SRNode nowDANode = findSRNode(myTree, nowNode.nodeNum);
+			int nowMoney = OntologyManager.getMoney(nowDANode.values.Sensor);
+			int nowTime = OntologyManager.getTime(nowDANode.values.Sensor);
+			
+			nowCost = nowMoney*Wm + nowTime*Wt;
+			
+			Costs[nowNode.nodeNum] = nowCost;
+			
+			leafNodeNum.add(leafNum);
+			LeafNode[nowNode.nodeNum] = leafNodeNum;
+			//System.out.println("calcul: now num " + nowNode.nodeNum + ", nowCost = " + nowCost);
+			calcul(myTree, nowNode.Parents.get(0), nowCost, leafNum, leafNodeNum);
+		}
 		
+		if(nowNode.type == types.typePSR || nowNode.type == types.typeVSR || nowNode.type == types.typeAC) { // if PSR or VSR, it is just middle of way, so update and continue.
+			Costs[nowNode.nodeNum] = nowCost;
+			LeafNode[nowNode.nodeNum] = leafNodeNum;
+			//System.out.println("calcul: now num " + nowNode.nodeNum + ", nowCost = " + nowCost);
+			calcul(myTree, nowNode.Parents.get(0), nowCost, leafNum, leafNodeNum);
+		}
+		
+		if(nowNode.type == types.typeX) {
+			int xCost = Costs[nowNode.nodeNum];
+			if(nowCost < xCost) {
+				Costs[nowNode.nodeNum] = nowCost;
+				LeafNode[nowNode.nodeNum] = leafNodeNum;
+			}
+			else {
+				nowCost = xCost;
+			}
+			//System.out.println("calcul: now num " + nowNode.nodeNum + ", nowCost = " + nowCost);
+			calcul(myTree, nowNode.Parents.get(0), nowCost, leafNum, leafNodeNum);
+		}
+		
+		if(nowNode.type == types.typePlus) { // if + node, it has to integrate each branch.
+			int plusCost = Costs[nowNode.nodeNum];
+			
+			if(plusCost >= MAX) {
+				Costs[nowNode.nodeNum] = nowCost;
+				LeafNode[nowNode.nodeNum] = leafNodeNum;
+			}
+			else {
+				Costs[nowNode.nodeNum] = plusCost + nowCost;
+				nowCost = plusCost + nowCost;
+				LeafNode[nowNode.nodeNum].add(leafNum);
+				leafNodeNum = LeafNode[nowNode.nodeNum];
+			}
+			//System.out.println("calcul: now num " + nowNode.nodeNum + ", nowCost = " + nowCost);
+			calcul(myTree, nowNode.Parents.get(0), nowCost, leafNum, leafNodeNum);
+		}
+		
+		if(nowNode.type == types.typeUR || nowNode.type == types.typeUA) { // if UR node, it is final node of this recursive function, so it has to find the cheapest one.
+			int URCost = Costs[nowNode.nodeNum];
+			if(nowCost < URCost) {
+				Costs[nowNode.nodeNum] = nowCost;
+				LeafNode[nowNode.nodeNum] = leafNodeNum;
+			}
+			else {
+				nowCost = URCost;
+			}
+			//System.out.println("feasible UR = " + nowNode.nodeNum + " " + LeafNode[nowNode.nodeNum]);
+			//System.out.println("calcul: now num " + nowNode.nodeNum + ", nowCost = " + nowCost);
+			return;
+		}
+	}
+	
+	public static void remove(Node nowNode) { // similar to previous remove algorithm
+		//System.out.println(nowNode.nodeNum);
+		if(nowNode.type == types.typeUR || nowNode.type == types.typeUA) return; // if UR or UA, it is final node, so return.
+		
+		if(nowNode.type == types.typeX || nowNode.type == types.typeXc) { // if x node, there will be another chance.
+			if(!nowNode.Children.isEmpty()) return;
+		}
+
+		// remove nowNode from children' parents array
+		ArrayList<Node> children = nowNode.Children;
+		if(!children.isEmpty()) {
+			for(int i = 0 ; i < children.size() ; i++) {
+				Node nowChild = children.get(i);
+				nowChild.Parents.remove(nowNode);
+			}
+		}
+		
+		// remove nowNode from parents' children array
+		ArrayList<Node> parents = nowNode.Parents;
+		for(int i = 0 ; i < parents.size() ; i++) {
+			Node nowPar = parents.get(i);
+			//System.out.println("nowPar " + nowPar.nodeNum);
+			nowPar.Children.remove(nowNode);
+			
+			// if parent is + node, remove all children
+			if(nowPar.type == types.typePlus) { 
+				//System.out.println("nowPar " + nowPar.nodeNum);
+				nowPar.Children.clear();
+				nowPar.isLeaf = nowNode.isLeaf;
+			}
+			
+			// if parent has empty children, remove parent
+			if(nowPar.Children.isEmpty()) {
+				//System.out.println("nowPar " + nowPar.nodeNum);
+				nowPar.isLeaf = nowNode.isLeaf;
+				remove(nowPar);
+			}
+		}
+		return;
 	}
 }
