27fde7d5ecc8f3fe9c6dfa792704340b9c976434
gpdms960823@gmail.com <gpdms960823@gmail.com>
2019-10-07 15:13:55 -0700

    update

--- tippersTree2/src/main/java/tippersTree/Statement.java
+++ tippersTree2/src/main/java/tippersTree/Statement.java
@@ -1,7 +1,25 @@
 package tippersTree;
 
+import java.util.ArrayList;
+
 public class Statement {
 	public String Sensor;
 	public String Observation;
 	public String Entity;
+	public ArrayList<Statement> Former = new ArrayList<Statement>(); 
+	
+	public Statement() {}
+	
+	public Statement(String newSensor, String newObservation,String newEntity) {
+		Sensor = newSensor;
+		Observation = newObservation;
+		Entity = newEntity;
+	}
+	
+	public Statement(String newSensor, String newObservation,String newEntity, ArrayList<Statement> newStatement) {
+		Sensor = newSensor;
+		Observation = newObservation;
+		Entity = newEntity;
+		Former = newStatement;
+	}
 }
--- tippersTree2/src/main/java/tippersTree/Tree_Execute.java
+++ tippersTree2/src/main/java/tippersTree/Tree_Execute.java
@@ -10,35 +10,32 @@ public class Tree_Execute extends Tree {
 			URNode nowURNode = findURNode(feasibleTree, URij.get(i).nodeNum); // get URNode
 			Stack<Node> st = reverseNodes(nowURNode);
 
+			Statement nowState = new Statement();
+			ArrayList<Statement> temp = new ArrayList<Statement>();
+
 			while (!st.isEmpty()) {
 				Node now = st.pop();
 				int nowNum = now.nodeNum;
 
-				if (now.type == types.typeX || now.type == types.typePlus) {
-					System.out.println(now.type);
-				} else if (now.type == types.typeDA) {
-					SRNode now2 = findSRNode(feasibleTree, nowNum);
-					System.out.println(now2.type + " <" + now2.nodeNum + "  " + now2.values.Entity + "  "
-							+ now2.values.Observation + "  " + now2.values.Sensor + ">");
-				} else if (now.type == types.typePSR || now.type == types.typeVSR) {
-					SRNode now2 = findSRNode(feasibleTree, nowNum);
-					System.out.println(now2.type + " <" + now2.nodeNum + "  " + now2.values.Entity + "  "
-							+ now2.values.Observation + "  " + now2.values.Sensor + ">");
-				} else if (now.type == types.typeUR) {
-					URNode now2 = findURNode(feasibleTree, nowNum);
-					System.out.println(now2.type + " <" + now2.nodeNum + "  " + now2.values.Entity + "  "
-							+ now2.values.Condition + ">");
-				}
+				SRNode now2 = findSRNode(feasibleTree, nowNum);
+				nowState = new Statement(now2.values.Sensor, now2.values.Observation, now2.values.Entity, temp);
+				
+				System.out.println(now2.type + " <" + nowState.Sensor + " " + nowState.Observation + " "
+						+ nowState.Entity + " " + nowState.Former + ">");
+//				System.out.println(now2.type + " <" + now2.nodeNum + "  " + now2.values.Entity + "  "
+//						+ now2.values.Observation + "  " + now2.values.Sensor + ">");
+				System.out.println(nowState);
+				
+				temp.add(nowState);
 			}
 		}
 	}
 
 	public static Stack<Node> reverseNodes(URNode nowURNode) {
-
 		Stack<Node> reverse = new Stack<Node>();
 		Queue<Node> temp = new LinkedList<Node>();
 
-		reverse.add(nowURNode); // add URNode to stack
+		// reverse.add(nowURNode); // add URNode to stack
 
 		for (int j = 0; j < nowURNode.Children.size(); j++) {
 			if (nowURNode.Children.get(j).type != types.typeXc)
@@ -46,13 +43,13 @@ public class Tree_Execute extends Tree {
 
 			while (!temp.isEmpty()) {
 				Node nowNode = temp.remove();
-
 				temp.addAll(nowNode.Children);
-				reverse.add(nowNode);
+
+				if (nowNode.type != types.typeX & nowNode.type != types.typePlus)
+					reverse.add(nowNode);
 			}
 		}
-		System.out.println("nodes" + reverse); // print nodeNum
-
+		// System.out.println("nodes" + reverse); // print nodeNum
 		return reverse;
 	}
 }
