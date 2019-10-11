4b6e15a5958bca17d138b94a91ea906190155020
gpdms960823@gmail.com <gpdms960823@gmail.com>
2019-10-10 17:17:57 -0700

    updated Tree_execute

--- tippersTree2/src/main/java/tippersTree/Tree_Execute.java
+++ tippersTree2/src/main/java/tippersTree/Tree_Execute.java
@@ -3,57 +3,61 @@ package tippersTree;
 import java.util.*;
 
 public class Tree_Execute extends Tree {
+	public static Tree myTree = new Tree();
+	public static ArrayList<Node> leaves = new ArrayList<Node>();
+	public static HashMap<Integer, Statement> checkDup = new HashMap<Integer, Statement>();
 
-	public static void executeTree(Tree feasibleTree, URNode nowURNode) {
+	public static Statement executeTree(Tree feasibleTree) {
+		myTree = feasibleTree;
 
-		// for (int i = 0; i < URij.size(); i++) {
-			// URNode nowURNode = findURNode(feasibleTree, URij.get(i).nodeNum); // get URNode
-		Stack<Node> reverse = reverseNodes(nowURNode);
+		Statement result = new Statement();
+		leaves = findLeafNode(feasibleTree);
+		for (Node leaf : leaves) {
+			if (leaf.Parents.get(0).Parents.get(0).type != types.typeAC) {
+				SRNode nowSR = findSRNode(myTree, leaf.nodeNum);
+				Statement state1 = new Statement(nowSR.values.Sensor, nowSR.values.Observation, nowSR.values.Entity);
 
-		String action = "PREPARE";
-		Statement nowState = new Statement();
-		ArrayList<Statement> temp = new ArrayList<Statement>();
+				System.out.println(nowSR.nodeNum + "	:	" + state1.Entity);
+				System.out.println(nowSR.nodeNum + "	:	" + state1.Sensor);
+				System.out.println(nowSR.nodeNum + "	:	" + state1.Observation);
+				System.out.println(nowSR.nodeNum + "	:	" + state1.Former);
+				System.out.println("===========" + nowSR.nodeNum + "	:	" + state1 + "===========");
 
-		while (!reverse.isEmpty()) {
-			Node now = reverse.pop();
-			int nowNum = now.nodeNum;
+				checkDup.put(nowSR.nodeNum, state1);
 
-			SRNode now2 = findSRNode(feasibleTree, nowNum);
-			nowState = new Statement(now2.values.Sensor, now2.values.Observation, now2.values.Entity, temp);
-
-			System.out.println(action + " <" + nowState.Sensor + ", " + nowState.Observation + ", " + nowState.Entity
-					+ ", " + nowState.Former + ">");
-
-			if (!reverse.iterator().hasNext()) {
-				action = "CALL";
-			} else {
-				temp.add(nowState);
+				System.out.println("이걸 해볼까" + createStatement(nowSR, state1));
 			}
 		}
-		System.out.println(action + " <" + nowState.Sensor + ", " + nowState.Observation + ", " + nowState.Entity + ", "
-				+ nowState.Former + ">");
+		return result;
 	}
-	// }
 
-	public static Stack<Node> reverseNodes(URNode nowURNode) {
-		Stack<Node> reverse = new Stack<Node>();
-		Queue<Node> temp = new LinkedList<Node>();
+	public static Statement createStatement(SRNode nowSR, Statement state1) {
+		Node parent = nowSR.Parents.get(0).Parents.get(0);
 
-		// reverse.add(nowURNode); // add URNode to stack
-
-		for (int j = 0; j < nowURNode.Children.size(); j++) {
-			if (nowURNode.Children.get(j).type != types.typeXc)
-				temp.add(nowURNode.Children.get(j));
-
-			while (!temp.isEmpty()) {
-				Node nowNode = temp.remove();
-				temp.addAll(nowNode.Children);
-
-				if (nowNode.type != types.typeX & nowNode.type != types.typePlus)
-					reverse.add(nowNode);
+		if (parent.type == types.typeUR) {
+			return state1;
+		} else {
+			Statement state2 = new Statement();
+			SRNode nowSR2 = findSRNode(myTree, parent.nodeNum);
+			if (checkDup.containsKey(nowSR2.nodeNum)) {
+				System.out.println("=== duplicated ===" + nowSR2.nodeNum);
+				state2 = checkDup.get(nowSR2.nodeNum);
+			} else {
+				state2 = new Statement(nowSR2.values.Sensor, nowSR2.values.Observation, nowSR2.values.Entity);
 			}
+
+			state2.Former.add(state1);
+
+			checkDup.put(nowSR2.nodeNum, state2);
+
+			System.out.println(nowSR2.nodeNum + "	:	" + state2.Entity);
+			System.out.println(nowSR2.nodeNum + "	:	" + state2.Sensor);
+			System.out.println(nowSR2.nodeNum + "	:	" + state2.Observation);
+			System.out.println(nowSR2.nodeNum + "	:	" + state2.Former);
+			System.out.println("===========" + nowSR2.nodeNum + "	:	" + state2 + "===========");
+
+			createStatement(nowSR2, state2);
 		}
-		// System.out.println("nodes" + reverse); // print nodeNum
-		return reverse;
+		return state1;
 	}
 }
