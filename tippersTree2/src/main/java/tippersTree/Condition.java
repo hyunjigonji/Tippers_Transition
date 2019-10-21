b3d93e6ac30f9e39d4ec6cedc629226ed1fc9337
hyunjigonji <wew9955@naver.com>
2019-10-22 00:35:22 +0200

    completed condition

--- tippersTree2/src/main/java/tippersTree/Condition.java
+++ tippersTree2/src/main/java/tippersTree/Condition.java
@@ -3,88 +3,353 @@ package tippersTree;
 import java.util.*;
 
 public class Condition {
-	public ArrayList<String> conds = new ArrayList<String>(Arrays.asList("Occupancy","Capacity","Location","Connectivity", "Temperture")); // contains condition property
-	//public ArrayList<String> conjunction = new ArrayList<String>(Arrays.asList("if","when","where")); // contains conjunction like if, when, where
-	//public ArrayList<String> connect = new ArrayList<String>(Arrays.asList("and","or")); // contains connective like and, or -> AND/OR
-	public ArrayList<String> compare1 = new ArrayList<String>(Arrays.asList("is greater than", "is higher than", "is bigger than", "is better than", "is above", "is over")); // contains compare words which represents bigger left -> >
-	public ArrayList<String> compare2 = new ArrayList<String>(Arrays.asList("is more than"));
-	public ArrayList<String> compare3 = new ArrayList<String>(Arrays.asList("is smaller than", "is lower than", "is under", "is below")); // contains compare words which represents bigger right -> <
-	public ArrayList<String> compare4 = new ArrayList<String>(Arrays.asList("is less than"));
-	public ArrayList<String> compare5 = new ArrayList<String>(Arrays.asList("is same as", "is equal to")); // contains compare words which represents both same -> =
-	//public ArrayList<String> rate = new ArrayList<String>(Arrays.asList("% of", "percent of", "half of", "quarter of")); // contains rate words -> N%
+	public ArrayList<String> conds = new ArrayList<String>(Arrays.asList("Occupancy","Capacity","Location","Connectivity","Temperature")); // contains condition property
 	
-	String test = convertingCond("Occupancy is more than half of Capacity and Connectivity is lower than 10");
+	public ArrayList<String> Property = OntologyManager.getCondObs();
 	
-	public String convertingCond(String Cond) {
-		//Cond = Cond.toLowerCase();
-		String result = "";
+	public boolean test = calculCond("((Temperture+Connectivity)/2>10 || Occupancy<2*Capacity) && Temperture>40");
+	
+	public boolean calculCond(String input){ 
+		System.out.println("START : " + input);
+		// 프로퍼티 모두 숫자로 변환 
+		for(int i = 0 ; i < Property.size() ; i++) { 
+			//System.out.println("now prop = " + Property.get(i));
+			if(input.contains(Property.get(i))) {
+				String nowProp = Property.get(i);
+				int nowNum = extractReal(nowProp);
+				input = input.replaceAll(nowProp, Integer.toString(nowNum));
+			}
+		}
+		System.out.println("STEP 1 : " + input);
 		
-		while(Cond.contains("and") || Cond.contains("or")) {
-			int index = Cond.contains("and") ? Cond.indexOf("and") : Cond.indexOf("or");
-			if(index == 0) index = Cond.length();
-			String nowCond = Cond.substring(0, index);
-			//System.out.println("now Cond111 = " + nowCond);
-			if(nowCond.contains("and")) nowCond = nowCond.replaceAll("and", "AND");
-			if(nowCond.contains("or")) nowCond = nowCond.replaceAll("or", "OR");
-			//System.out.println("now Cond = " + nowCond);
+		// AND, OR, 부등호로 파싱해서 사칙연산하기 
+		StringTokenizer token = new StringTokenizer(input, "><=!&|");
+		while(token.hasMoreTokens()) {
+			String now = token.nextToken();
+			//System.out.println("nownownowno " + now);
+			String nowExp = processing(now); // 띄어쓰기나 괄호 있으면 없애기 
+			//System.out.println("STEP 2 : " + nowExp);
 			
-			for(int i = 0 ; i < compare1.size() ; i++) { // check if it contains greater or higher
-				if(nowCond.contains(compare1.get(i))) {
-					nowCond = nowCond.replace(compare1.get(i), ">");
-					break;
+			int nowResult = calculator(nowExp); // 사칙연산 계산하기 
+			input = input.replace(nowExp, Integer.toString(nowResult));
+			//System.out.println(nowExp);
+		}
+		System.out.println("STEP 2 : " + input);
+		
+		// AND, OR 로 파싱해서 부등호 계산하기 
+		StringTokenizer token2 = new StringTokenizer(input, "&|");
+		while(token2.hasMoreTokens()) {
+			String now2 = token2.nextToken();
+			
+			String nowExp2 = processing(now2);
+			//System.out.println("STEP 4 : " + nowExp2);
+			
+			String nowResult2 = calculator3(nowExp2);
+			input = input.replace(nowExp2, nowResult2);
+			//System.out.println(nowExp2);
+		}
+		System.out.println("STEP 3 : " + input);
+		
+		boolean result = calculator2(input);
+		
+		return result;
+	}
+	
+	public String processing(String input) {
+		if(input.contains(" ")) input = input.replaceAll(" ", "");
+		
+		int brackets = 0;
+		for(int i = 0 ; i < input.length() ; i++) {
+			if(input.charAt(i) == '(') brackets++;
+			else if(input.charAt(i) == ')') brackets--;
+		}
+		if(brackets == 0) return input;
+		else if(brackets < 0) { // )가 더 많음 
+			brackets = Math.abs(brackets);
+			for(int i = 0 ; i < brackets ; i++) {
+				for(int j = input.length()-1 ; j >= 0 ; j--) {
+					if(input.charAt(j) == ')') {
+						input = input.replace(Character.toString(input.charAt(j)),"");
+						continue;
+					}
 				}
 			}
-			for(int i = 0 ; i < compare2.size() ; i++) { // check if it contains greater or higher
-				if(nowCond.contains(compare2.get(i))) {
-					nowCond = nowCond.replace(compare2.get(i), ">=");
-					break;
-				}
+		}
+		else {
+			for(int i = 0 ; i < brackets ; i++) {
+				input = input.replaceFirst("[(]", "");
 			}
-			for(int i = 0 ; i < compare3.size() ; i++) { // check if it contains smaller or lower
-				if(nowCond.contains(compare3.get(i))) {
-					nowCond = nowCond.replace(compare3.get(i), "<");
-					break;
-				}
-			}
-			for(int i = 0 ; i < compare4.size() ; i++) { // check if it contains greater or higher
-				if(nowCond.contains(compare4.get(i))) {
-					nowCond = nowCond.replace(compare4.get(i), "<=");
-					break;
-				}
-			}
-			for(int i = 0 ; i < compare5.size() ; i++) { // check if it contains same
-				if(nowCond.contains(compare5.get(i))) {
-					nowCond = nowCond.replace(compare5.get(i), "=");
-					break;
-				}
+		}
+		//System.out.println("processing after = " + input);
+		return input;
+	}
+	
+	public ArrayList<String> makePostfix(String input){
+		Stack<Character> stack = new Stack<Character>();
+		ArrayList<String >newExp = new ArrayList<String>();
+		
+		String num = "";
+		for(int i = 0 ; i < input.length() ; i++) {
+			char nowChar = input.charAt(i);
+			if (nowChar == ' ') continue;
+			
+			else if(Character.isDigit(nowChar) || nowChar == '.') { // 숫자가 여러자리 수일 수 있으니까 저장 
+				num += nowChar;
 			}
 			
-			// convert rate to number
-			int ind = 0; // index which points to word representing rate
-			if(nowCond.contains("half of")) {
-				nowCond = nowCond.replace("half of", "0.5 *");
-			}
-			else if(nowCond.contains("quarter of")) {
-				nowCond = nowCond.replace("quarter of", "0.25 *");
-			}
-			else if(nowCond.contains("% of") || nowCond.contains("percent of")) {
-				ind = nowCond.contains("% of")? nowCond.indexOf("% of") : nowCond.indexOf("percent of"); // value is index of % or p
-				String left = nowCond.substring(0, ind);
-				left = left.replaceAll("[^0-9]",""); // erase all letters except numbers
-				//System.out.println(left);
-				int leftNum = Integer.parseInt(left); 
-				double realNum = 0.01*leftNum; // number of rate
+			else {
+				if(!num.isEmpty()) { // 숫자 어레이에 넣기 
+					newExp.add(num);
+					num = "";
+				}
 				
-				nowCond = nowCond.replaceAll(left, Double.toString(realNum)+" *");
-				nowCond = nowCond.replace(nowCond.contains("% of")?"% of":"percent of", "");
+				if(nowChar == '(') { // 그냥 스택에 푸쉬 
+					stack.push(nowChar);
+				}
+				else if(nowChar == ')') { // (를 만날 때까지 출력 
+					char stackChar = stack.pop();
+					while(stackChar != '(' && !stack.isEmpty()) {
+						newExp.add(Character.toString(stackChar));
+						stackChar = stack.pop();
+					}
+				}
+				else if(nowChar == '+' || nowChar == '-') {
+					if(stack.isEmpty()) {
+						stack.push(nowChar);
+					}
+					else {
+						while(!stack.isEmpty()) {
+							char stackChar = stack.pop();
+						
+							if(stackChar == '(') {
+								stack.push(stackChar);
+								stack.push(nowChar);
+								break;
+							}
+							else {
+								newExp.add(Character.toString(stackChar));
+							}
+						}
+					}
+				}
+				else if(nowChar == '/' || nowChar == '*') {
+					if(stack.isEmpty()) {
+						stack.push(nowChar);
+					}
+					else {
+						while(!stack.isEmpty()) {
+							char stackChar = stack.pop();
+						
+							if(stackChar == '(') {
+								stack.push(stackChar);
+								stack.push(nowChar);
+								break;
+							}
+							else if(stackChar == '*' || stackChar == '/') {
+								newExp.add(Character.toString(stackChar));
+							}
+							else {
+								stack.push(nowChar);
+								break;
+							}
+						}
+					}
+				}
 			}
-			result += nowCond;
-			//System.out.println("result = " + result);
+		}
+		if(!num.isEmpty()) { // 숫자 어레이에 넣기 
+			newExp.add(num);
+			num = "";
+		}
+		while(!stack.empty()) newExp.add(Character.toString(stack.pop()));
+		//System.out.println(newExp);
+		
+		return newExp;
+	}
+	
+	public int calculator(String input) { // 괄호, 사칙연산 처리하는 계산기 + 만약 사칙연산이 없으면 값을 그대로 리
+		// 후위표기법으로 변환 
+		ArrayList<String> postfix = makePostfix(input);
+		int i = 0;
+		while(postfix.size() > 2) {
+			String left = postfix.get(i);
+			String right = postfix.get(i+1);
+			String oper = postfix.get(i+2);
+				
+			if(oper.equals("+") || oper.equals("-") || oper.equals("*") || oper.equals("/")) {
+				int leftNum = Integer.parseInt(left);
+				int rightNum = Integer.parseInt(right);
+					
+				int resultNum = 0;
+				if(oper.equals("+")) resultNum = leftNum + rightNum;
+				else if(oper.equals("-")) resultNum = leftNum - rightNum;
+				else if(oper.equals("*")) resultNum = leftNum * rightNum;
+				else if(oper.equals("/")) resultNum = leftNum / rightNum;
+					
+				postfix.remove(i);
+				postfix.remove(i);
+				postfix.remove(i);
+	
+				postfix.add(i, Integer.toString(resultNum));
+				
+				i = 0;
+			}
+			else {
+				i++;
+			}
+		}
+		int result = Integer.parseInt(postfix.get(0));
+		// System.out.println("result === " + result);
+		return result;
+	}
+	
+	public ArrayList<String> makePostfix2(String input) {
+		Stack<Character> stack = new Stack<Character>();
+		ArrayList<String> newExp = new ArrayList<String>();
+		
+		String bool = "";
+		for(int i = 0 ; i < input.length() ; i++) {
+			char nowChar = input.charAt(i);
+			if(nowChar == ' ') continue;
 			
-			Cond = Cond.substring(index, Cond.length());
-			//System.out.println("Cond = " + Cond);
+			else if(Character.isAlphabetic(nowChar)) {
+				bool += nowChar;
+			}
+			
+			else {
+				if(!bool.isEmpty()) { // 숫자 어레이에 넣기 
+					newExp.add(bool);
+					bool = "";
+				}
+				
+				if(nowChar == '(') { // 그냥 스택에 푸쉬 
+					stack.push(nowChar);
+				}
+				else if(nowChar == ')') { // (를 만날 때까지 출력 
+					char stackChar = stack.pop();
+					while(stackChar != '(' && !stack.isEmpty()) {
+						newExp.add(Character.toString(stackChar));
+						stackChar = stack.pop();
+					}
+				}
+				else if(nowChar == '&' || nowChar == '|') {
+					if(stack.isEmpty()) {
+						stack.push(nowChar);
+					}
+					else {
+						while(!stack.isEmpty()) {
+							char stackChar = stack.pop();
+						
+							if(stackChar == '(') {
+								stack.push(stackChar);
+								stack.push(nowChar);
+								break;
+							}
+							else {
+								newExp.add(Character.toString(stackChar));
+							}
+						}
+					}
+					i++;
+				}
+			}
+		}
+		if(!bool.isEmpty()) { // 숫자 어레이에 넣기 
+			newExp.add(bool);
+			bool = "";
+		}
+		while(!stack.empty()) newExp.add(Character.toString(stack.pop()));
+		//System.out.println(newExp);
+		
+		return newExp;
+	}
+	
+	public Boolean calculator2(String input) { // 부등호 계산 
+		ArrayList<String> postfix = makePostfix2(input);
+		int i = 0;
+		while(postfix.size() > 2) {
+			String left = postfix.get(i);
+			String right = postfix.get(i+1);
+			String oper = postfix.get(i+2);
+				
+			if(oper.equals("&") || oper.equals("|")) {
+				String result = "FALSE";
+				
+				if(oper.equals("&")) {
+					if(left.equals("TRUE") == true && right.equals("TRUE")) result = "TRUE";
+				}
+				if(oper.equals("|")) {
+					if(left.equals("TRUE") || right.equals("TRUE")) result = "TRUE";
+				}
+				
+				postfix.remove(i);
+				postfix.remove(i);
+				postfix.remove(i);
+	
+				postfix.add(i,result);
+				
+				i = 0;
+			}
+			else {
+				i++;
+			}
+		}
+		String result = postfix.get(0);
+		System.out.println("STEP 4 : " + result);
+		
+		boolean resultBool = false;
+		if(result.equals("TRUE")) resultBool = true;
+		
+		return resultBool;
+	}
+	
+	public String calculator3(String input) {
+		int ind = 0;
+		String result = "FALSE";
+		if(input.contains(">")) {
+			ind = input.indexOf(">");
+			
+			int left = Integer.parseInt(input.substring(0,ind-1));
+			int right = Integer.parseInt(input.substring(ind+1, input.length()-1));
+			
+			if(left > right) result = "TRUE";
+		}
+		else if(input.contains(">=")) {
+			ind = input.indexOf(">=");
+			
+			int left = Integer.parseInt(input.substring(0,ind-1));
+			int right = Integer.parseInt(input.substring(ind+2, input.length()-1));
+			
+			if(left >= right) result = "TRUE";
+		}
+		else if(input.contains("<")) {
+			ind = input.indexOf("<");
+			
+			int left = Integer.parseInt(input.substring(0,ind-1));
+			int right = Integer.parseInt(input.substring(ind+1, input.length()-1));
+			
+			if(left < right) result = "TRUE";
+		}
+		else if(input.contains("<=")) {
+			ind = input.indexOf("<=");
+			
+			int left = Integer.parseInt(input.substring(0,ind-1));
+			int right = Integer.parseInt(input.substring(ind+2, input.length()-1));
+			
+			if(left > right) result = "TRUE";
 		}
 		
 		return result;
 	}
+	
+	public int extractReal(String input) {
+		if(input.equals("Location")) return 15;
+		else if(input.equals("Occupancy")) return 20;
+		else if(input.equals("Capacity")) return 25;
+		else if(input.equals("Connectivity")) return 30;
+		else if(input.equals("Temperture")) return 35;
+		else if(input.equals("Image")) return 40;
+		else return 0;
+	}
+	
 }
--- tippersTree2/src/main/java/tippersTree/OntologyManager.java
+++ tippersTree2/src/main/java/tippersTree/OntologyManager.java
@@ -31,7 +31,7 @@ public class OntologyManager {
 		String Cond = UARequest.Condition;
 		
 		Condition conditions = new Condition();
-		// System.out.println(conditions.test);
+		System.out.println(conditions.test);
 		for(int i = 0 ; i < conditions.conds.size() ; i++) {
 			String nowCon = conditions.conds.get(i);
 			if(Cond.contains(nowCon)) {
@@ -279,5 +279,10 @@ public class OntologyManager {
 //		
 //		return 10;
 		return tippersOntology.OntologyManager.getTime(Sen);
+	}
+	
+	public static ArrayList<String> getCondObs(){
+		
+		return tippersOntology.OntologyManager.getObservation();
 	}
 }
